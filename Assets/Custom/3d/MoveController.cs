d6aa839f1660bef6c5fc57ac9ff3b037d23d3649
shadow <aaa>
2022-11-09 16:37:49 +0800

    3d

--- Assets/Custom/3d/MoveController.cs
+++ Assets/Custom/3d/MoveController.cs
@@ -1,15 +1,22 @@
+using System;
 using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private int _air;
+    private Vector3 _speed;
     private Transform _camera;
+    private Animator _animator;
+    private static readonly int Jump = Animator.StringToHash("jump");
+    private static readonly int Fall = Animator.StringToHash("fall");
+    private static readonly int Speed = Animator.StringToHash("speed");
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _camera = GetComponentInChildren<Camera>().transform;
+        _animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -22,17 +29,31 @@ public class MoveController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space) && _air > 0)
         {
-            _rigidbody.AddForce(0, 10, 0);
+            _rigidbody.AddForce(transform.rotation * _speed * 5 + new Vector3(0, 10, 0));
+            _animator.SetTrigger(Jump);
+        }
+
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = true;
         }
 
         _camera.parent.transform.Rotate(-Input.GetAxis("Mouse Y"), 0, 0);
-        _camera.parent.parent.transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+        transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
         _camera.transform.localPosition += Vector3.forward * Input.GetAxis("Mouse ScrollWheel");
     }
 
     private void FixedUpdate()
     {
-        transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+        if (_air == 0) return;
+        // transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
         var f = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
         {
@@ -43,30 +64,47 @@ public class MoveController : MonoBehaviour
             f += Vector3.back;
         }
 
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.A))
         {
             f += Vector3.left;
         }
-        else if (Input.GetKey(KeyCode.E))
+        else if (Input.GetKey(KeyCode.D))
         {
             f += Vector3.right;
         }
 
+        _speed = f.normalized;
         if (!(f.magnitude > 0)) return;
-        f.Normalize();
+        _animator.SetFloat(Speed, f.magnitude);
         var transform1 = transform;
-        transform1.position += transform1.rotation * f / 8f;
+        transform1.position += transform1.rotation * _speed / 8f;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        _air++;
-        Debug.Log("OnCollisionEnter" + _air);
+        // _air++;
+        // Debug.Log("OnCollisionEnter" + _air);
     }
 
     private void OnCollisionExit(Collision other)
     {
+        // _air--;
+        // Debug.Log("OnCollisionExit" + _air);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _air++;
+        Debug.Log("OnTriggerEnter" + _air);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
         _air--;
-        Debug.Log("OnCollisionExit" + _air);
+        Debug.Log("OnTriggerExit" + _air);
+        if (_air == 0)
+        {
+            _animator.SetTrigger(Fall);
+        }
     }
 }
