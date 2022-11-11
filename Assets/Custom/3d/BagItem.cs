653a57eb66756102404005766988730b0203fca4
shadow <aaa>
2022-11-11 14:29:02 +0800

    item

--- Assets/Custom/3d/BagItem.cs
+++ Assets/Custom/3d/BagItem.cs
@@ -2,20 +2,28 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class BagItem : MonoBehaviour, IPointerDownHandler
+public class BagItem : MonoBehaviour
 {
+    private int _id;
+    private int _count;
     public Text id;
     public Text count;
 
-    public void OnPointerDown(PointerEventData eventData)
+    public void ShowInfo()
     {
-        EventManager.Instance.DispatchEvent("evt_open_item_info");
+        EventManager.Instance.DispatchEvent("evt_open_item_info", _id);
+        Debug.Log(id.text);
+        Debug.Log(count.text);
+        Debug.Log(_id);
+        Debug.Log(_count);
     }
 
-    public void SetInfo(int _id, int _count)
+    public void SetInfo(int itemId, int itemCount)
     {
-        id.text = _id.ToString();
-        count.text = _count.ToString();
-        count.gameObject.SetActive(_count > 1);
+        _id = itemId;
+        _count = itemCount;
+        id.text = itemId.ToString();
+        count.text = itemCount.ToString();
+        count.gameObject.SetActive(itemCount > 1);
     }
 }
--- Assets/Custom/3d/ItemInfo.cs
+++ Assets/Custom/3d/ItemInfo.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemInfo : MonoBehaviour
 {
+    public Text Name;
+    public Text Info;
+
     private void Awake()
     {
-        EventManager.Instance.AddEventListener("evt_open_item_info", a => { gameObject.SetActive(true); });
+        EventManager.Instance.AddEventListener("evt_open_item_info", SetInfo);
         gameObject.SetActive(false);
     }
 
     public void Close()
     {
         gameObject.SetActive(false);
+    }
+
+    private void SetInfo(params object[] param)
+    {
+        gameObject.SetActive(true);
+        Name.text = param[0].ToString();
+        Info.text = param[0].ToString() + "info";
     }
 }
